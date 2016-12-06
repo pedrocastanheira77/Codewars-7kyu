8d3fd28b9b87552ce16c92c63c63d7d5e8b69c40
Pedro Castanheira <josepedrocastanheira@gmail.com>
2016-12-06 00:45:55 +0000

    Codewars Kata 7kyu

---
+++ The _mean_of_two_means.rb
@@ -0,0 +1,22 @@
+def mean_calculator array, elements
+  acm = 0
+  counter = 0
+  array.each do |arr_element|
+    break if counter >= elements
+    acm += arr_element
+    counter += 1
+  end
+  acm.to_f/elements
+end
+
+def getMean arr, x, y
+  if (x <= 1 || x > arr.size || y <= 1 || y > arr.size)
+    return -1
+  else
+    first_mean = mean_calculator arr, x
+    last_mean = mean_calculator arr.reverse, y
+    mean_of_means = ((last_mean + first_mean)/2).to_f
+  end
+end
+
+p getMean([1,3,2],2,2)
